f6a9cc7d60de8529b9b5577a9705d5cb168b56f9
nikitaolenych123-coder <nikitaolenych123@gmail.com>
2025-12-30 09:31:53 +0000

    chore: Remove build directories from git tracking
    - Remove cached build/ directory (CMake artifacts)
    - .gitignore already excludes build/ directories
    - Allows fresh builds without file exists errors
    - GitHub Actions can now create build directory cleanly

--- build/CMakeFiles/pxs3c_core.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pxs3c_core.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/pxs3c_core.dir/src/core/Config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pxs3c_core.dir/src/core/Config.cpp.o: \
- /workspaces/pxs3c/src/core/Config.cpp /usr/include/stdc-predef.h \
- /workspaces/pxs3c/src/core/Config.h
